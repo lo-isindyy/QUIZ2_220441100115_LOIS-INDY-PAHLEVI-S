d5fa937a40fcae4a9a7a98eacd2f32eae6487859
lo-isindyy <113223762+lo-isindyy@users.noreply.github.com>
2023-11-16 17:01:23 +0700

    Add files via upload

---
+++ src/packageTugas2/koneksi.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package packageTugas2;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class koneksi {
+    Connection con;
+    Statement stm;
+    
+    public void config(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost/db_login", "root", "");
+            stm = con.createStatement();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "koneksi gagal "+e.getMessage());
+        }
+    }
+}
